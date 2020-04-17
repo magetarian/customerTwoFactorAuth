e1bd48a1771daabfdcc46a4fa5b1288834f302ce
Alex Lukyanau <cybergomel@gmail.com>
2020-04-16 23:03:29 -0400

    [Frontend] Customer Configuration to Enable 2FA Issue #2

---
+++ Controller/Customer/Configuration.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @author     Sashas IT Support <support@sashas.org>
+ * @copyright  2020  Sashas IT Support Inc. (http://www.extensions.sashas.org)
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+declare(strict_types = 1);
+
+namespace Magetarian\CustomerTwoFactorAuth\Controller\Customer;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * Class Configuration
+ */
+class Configuration extends Action
+{
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    /**
+     * Configuration constructor.
+     *
+     * @param Context $context
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession
+    ) {
+        parent::__construct($context);
+        $this->customerSession = $customerSession;
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @param RequestInterface $request
+     * @return ResponseInterface
+     * @throws NotFoundException
+     */
+    public function dispatch(RequestInterface $request)
+    {
+        if (!$this->customerSession->authenticate()) {
+            $this->_actionFlag->set('', 'no-dispatch', true);
+        }
+        return parent::dispatch($request);
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->set(__('2FA Configuration'));
+        return $resultPage;
+    }
+}
---
+++ Model/Config/ConfigProvider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author     Sashas IT Support <support@sashas.org>
+ * @copyright  2020  Sashas IT Support Inc. (http://www.extensions.sashas.org)
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+declare(strict_types = 1);
+
+namespace Magetarian\CustomerTwoFactorAuth\Model\Config;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class ConfigProvider
+ */
+class ConfigProvider
+{
+    const XML_PATH_ENABLED = 'msp_securitysuite_twofactorauth/general/enabled_customer';
+    const XML_PATH_FORCE_PROVIDERS = 'msp_securitysuite_twofactorauth/general/force_providers_customer';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * ConfigProvider constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return !!$this->scopeConfig->getValue(static::XML_PATH_ENABLED, ScopeInterface::SCOPE_WEBSITE);
+    }
+
+    /**
+     * @return array
+     */
+    public function getForcedProviders(): array
+    {
+        $providers = $this->scopeConfig->getValue(static::XML_PATH_FORCE_PROVIDERS, ScopeInterface::SCOPE_WEBSITE);
+        return $providers ? explode(',', $providers) : [];
+    }
+}
