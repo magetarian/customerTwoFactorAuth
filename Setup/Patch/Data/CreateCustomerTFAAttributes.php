2fbe6d0b05f1e17b87c2349bb43923b4e14da74b
Alex Lukyanau <cybergomel@gmail.com>
2020-05-15 02:28:31 -0400

    Code Style Updates

--- Setup/Patch/Data/CreateCustomerTFAAttributes.php
+++ Setup/Patch/Data/CreateCustomerTFAAttributes.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author     The S Group <support@sashas.org>
+ * @copyright  2020  Sashas IT Support Inc. (https://www.sashas.org)
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
 declare(strict_types = 1);
 
 namespace Magetarian\CustomerTwoFactorAuth\Setup\Patch\Data;
